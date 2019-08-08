9d502c27e106cd2b08f832b27faa9d90a9bd3865
Lucas Dalmarco <dalmarcolucas@gmail.com>
2019-08-07 21:54:23 -0300

    Starting backend

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://root:UnXuPq7Yllj9HZPp@cluster0-4by1w.mongodb.net/tindev?retryWrites=true&w=majority',  {
+    useNewUrlParser: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
